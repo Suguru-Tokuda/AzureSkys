37f06597537343cb6968e961a6be3ae2beae16c0
Suguru Tokuda <suguru.tokuda@gmail.com>
2023-11-27 11:53:09 -0500

    Initial Commit

---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Suguru Tokuda on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
